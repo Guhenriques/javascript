517d8e2106df17cdb74ec2244dccf65b7f9c70db
Gustavo Henriques <guhenriques1398@gmail.com>
2022-04-02 15:14:38 -0300

    novos desafios e oganizacao

---
+++ exercicios/aula 13 - Repetiçoes/script.js
@@ -0,0 +1,26 @@
+// estrutura de repetiçao com teste lógico no início, testa, executa e faz looping
+var c = 1
+while (c <= 100) {
+  console.log(`Passo ${c}`)
+  c++
+}  
+
+
+/* estrutura de repetiçao com teste lógico no final
+
+var c = 1 
+do {
+  console.log(`Passo ${c}`)
+  c++
+} while (c <= 100)
+
+*/
+
+
+/* muito melhor que:
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+*/
